155075f51a4d887cce32c683b6f803dce59de2e6
Ayush Kumar <anonymouskmr@gmail.com>
2018-05-26 12:16:22 +0530

    mordified CSS,image,JS
    used real fruits pics

--- jquery.js
+++ jquery.js
@@ -7,7 +7,7 @@
     //change button text to "reset game"
     //1.create a random fruit
     //define a random step
-    //2.move fruit dwon one step every 30s
+    //2.move fruit down one step every 30s
     //is fruit too low?
       //no->repeat 2
       //yes-> any trials left?
@@ -17,76 +17,101 @@
 //slice a fruit
 //play sound in the background
 //explode fruit
+
 var playing = false;
 var score;
 var trialsleft;
 var fruits = ['apple', 'banana', 'cherries', 'grapes', 'mango', 'orange', 'peach', 'pear', 'watermelon'];
-var step;
+//var step=0.9; 
 var action;
 $(function(){
 
 $("#startreset").click(function(){
 
    if(playing == true){
+	   // resetting screen 
+	   playing = false;
+	   
        location.reload();
    }
    else{
+	   // starting game
        playing = true;
+	   
        score = 0;
-       $("#score").html(score);
+       $("#score").html(score*10);
        $("#trialsleft").show();
        trialsleft = 3;
        addHearts();
-       $("#startreset").html("Reset Game");
+       $("#startreset").html('RESET GAME');
        $("#gameover").hide();
-       startaction();
+       
+	   startaction();
+	   
    }
 }); 
 
 $("#fruit1").mouseover(function(){
    score += 1;
-   $("#score").html(score);
+   $("#score").html(score*10);
    $("#slicesound")[0].play();
    clearInterval(action);
-   $("#fruit1").hide("explode", 500);
+   $("#fruit1").hide("explode", 500); 
    startaction();
-   setTimeout(startaction, 500);
+   setTimeout(startaction, 500); // Time between two fruit after fruit explodes
 });
 
 function addHearts(){
+	
 $("#trialsleft").empty();
 for(i = 0; i < trialsleft; i++){
-           $("#trialsleft").append('<img src="images/heart.png" class="life">');
+           $("#trialsleft").append('<i class="fa fa-heart life"></i> ');
+		  
 }
 }
+
+/*
+startaction function
+
+show #fruit1, choose a random fruit , give initial position,
+*
+
+
+
+*/
+
+
 
 function startaction(){
 $("#fruit1").show();
 chooseFruit();
 $("#fruit1").css({'left': Math.round(550*Math.random()), 'top': -50});
-step = 1+Math.round(5*Math.random());
+//step = 1+Math.round(5*Math.random());
 action = setInterval(function(){
-    $("#fruit1").css('top', $("#fruit1").position().top + step);
+    $("#fruit1").css('top', $("#fruit1").position().top + 1);
     if($("#fruit1").position().top > $("#fruitscontainer").height()){
        if(trialsleft > 1){
            $("#fruit1").show();
            chooseFruit();
            $("#fruit1").css({'left': Math.round(550*Math.random()), 'top': -50});
-           step = 1+Math.round(5*Math.random());
+           //step = 1+Math.round(5*Math.random());
            trialsleft -= 1;
            addHearts();
        }
        else{
            $("#gameover").show();
            playing = false;
-           $("#startreset").html("Start Game");
-           $("#gameover").html('<p>Game Over!</p><p>Your score is '+ score +'</p>');
+           $("#startreset").html(' START GAME');
+           $("#gameover").html('<p class="over">Game Over!</p><p class="over"> Score:'+ score*10 +'</p>');
+		   trialsleft -= 1;
+           addHearts();
            stopaction();
        }}
 }, 10);
 }
 
 function chooseFruit(){
+	//console.log(step);
 $("#fruit1").attr('src','images/'+fruits[Math.round((Math.random()*8))]+'.png');
 }
 
